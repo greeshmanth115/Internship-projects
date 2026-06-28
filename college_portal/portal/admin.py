7a6103b81e4d46c19e9046577be7dc139ea8951d
greeshmanth115 <greeshmanth115@gmail.com>
2026-06-28 09:55:44 +0530

    add backend

--- college_portal/portal/admin.py
+++ college_portal/portal/admin.py
@@ -1,3 +1,12 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Student, Faculty, Course, Subject, Marks, Attendance, Notice, Timetable
+
+admin.site.register(Student)
+admin.site.register(Faculty)
+admin.site.register(Course)
+admin.site.register(Subject)
+admin.site.register(Marks)
+admin.site.register(Attendance)
+admin.site.register(Notice)
+admin.site.register(Timetable)
